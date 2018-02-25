dd32fd3646eec53ad3d0d979d96c9275821595cb
Otto <o.stegmaier@gmail.com>
2018-02-24 16:08:53 -0800

    Add tests.

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const morgan = require('morgan');
-const PORT = (process.env.PORT || 8080)
 const blogPostRouter = require('./blogPostRouter');
 
 function logErrors(err, req, res, next) {
@@ -16,7 +15,38 @@ app.get('/', (req, res) => res.send('nothing to see here.'));
 app.use('/blog-posts', blogPostRouter);
 app.use(logErrors);
 
+//common server object for run/close server ops.
+let server;
+function runServer() {
+  const PORT = (process.env.PORT || 8080)
+  return new Promise((resolve, reject) => {
+    server = app.listen(PORT, () => {
+      console.log(`App listening on port ${PORT}`);
+      resolve(server);
+    }).on('error', err => {
+      reject(err)
+    });  
+  });
+}
+  
+function closeServer() {
+  return new Promise((resolve, reject) => {
+    console.log('Closing server');
+    server.close(err => {
+      if (err) {
+        reject(err);
+        // so we don't also call `resolve()`
+        return;
+      }
+      resolve();
+    });
+  });
+}
 
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`);
-});
+// if server.js is called directly (aka, with `node server.js`), this block
+// runs. but we also export the runServer command so other code (for instance, test code) can start the server as needed.
+if (require.main === module) {
+  runServer().catch(err => console.error(err));
+};
+
+module.exports = {app, runServer, closeServer};
